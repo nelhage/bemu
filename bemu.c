bd4375f0f726160f656a0ecd6942686dd0f00965
Nelson Elhage <nelhage@mit.edu>
2008-06-29 00:57:51 -0700

    bemu.c: Add command-line options
    We support flags for emulation, timing, and dumping final state.

--- bemu.c
+++ bemu.c
@@ -1,16 +1,71 @@
 #include "bemu.h"
+#include <unistd.h>
+#include <getopt.h>
+
+#include <time.h>
+
+/* Subtract the `struct timeval' values X and Y,
+   storing the result in RESULT.
+   Return 1 if the difference is negative, otherwise 0.  */
+int
+timeval_subtract (result, x, y)
+     struct timeval *result, *x, *y;
+{
+  /* Perform the carry for the later subtraction by updating y. */
+  if (x->tv_usec < y->tv_usec) {
+    int nsec = (y->tv_usec - x->tv_usec) / 1000000 + 1;
+    y->tv_usec -= 1000000 * nsec;
+    y->tv_sec += nsec;
+  }
+  if (x->tv_usec - y->tv_usec > 1000000) {
+    int nsec = (x->tv_usec - y->tv_usec) / 1000000;
+    y->tv_usec += 1000000 * nsec;
+    y->tv_sec -= nsec;
+  }
+
+  /* Compute the time remaining to wait.
+     tv_usec is certainly positive. */
+  result->tv_sec = x->tv_sec - y->tv_sec;
+  result->tv_usec = x->tv_usec - y->tv_usec;
+
+  /* Return 1 if result is negative. */
+  return x->tv_sec < y->tv_sec;
+}
+
 
 int main(int argc, char **argv)
 {
     int fd;
+    int arg;
     struct stat stat;
+    struct timeval start, end, delta;
+    char *filename;
+    bool emulate = 0;
+    bool do_time = 0;
+    bool dump = 0;
 
     if(argc < 2) {
         printf("Usage: %s <file.bin>\n", argv[0]);
         exit(-1);
     }
 
-    fd = open(argv[1], O_RDWR);
+    while((arg = getopt(argc, argv, "det")) > 0) {
+        switch(arg) {
+        case 'd':
+            dump = 1;
+            break;
+        case 't':
+            do_time = 1;
+            break;
+        case 'e':
+            emulate = 1;
+            break;
+        }
+    }
+
+    filename = argv[optind];
+
+    fd = open(filename, O_RDWR);
     if(fd < 0) {
         perror("open");
         exit(-1);
@@ -29,7 +84,32 @@ int main(int argc, char **argv)
     close(fd);
 
     bcpu_reset();
-    bt_run();
+
+    gettimeofday(&start, NULL);
+    if(emulate) {
+        while(!CPU.halt) {
+            bcpu_step_one();
+        }
+    } else {
+        bt_run();
+    }
+    gettimeofday(&end, NULL);
+
+    if(do_time) {
+        timeval_subtract(&delta, &end, &start);
+        printf("Executed in %ds.%dus\n", delta.tv_sec, delta.tv_usec);
+    }
+
+    if(dump) {
+        int i;
+        printf("[%08x] Done\n", CPU.PC);
+        for(i = 0; i < 32; i++) {
+            printf("[%02x] %08x ", i, CPU.regs[i]);
+            if(i % 4 == 3) {
+                printf("\n");
+            }
+        }
+    }
 
     munmap(beta_mem, stat.st_size);
 
