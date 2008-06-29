c961586557e9039676bc704fa5b36238e2b2ed9d
Nelson Elhage <nelhage@mit.edu>
2008-06-29 01:15:56 -0700

    Add a basic test harness for the test programs

---
+++ run-tests.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+BEMU=./bemu
+
+run_one() {
+    file="$1"
+
+    bt_run=$($BEMU "tests/$file.bin" -d 2>&1)
+    err=$!
+    if [ "$err" -ne 0 ]; then
+        echo "[$file] FAIL: return code $err" >&2
+        echo "$bt_run";
+        exit 1;
+    fi 
+    em_run=$($BEMU "tests/$file.bin" -de 2>&1)
+
+    shift
+    while [ $# -gt 0 ]; do
+        expect="$1"
+        shift
+        if ! echo "$bt_run" | grep -qF "$expect"; then
+            echo "[$file] FAIL: Expecting: '$expect'" >&2
+            echo "$bt_run";
+            exit 1;
+        fi
+    done
+
+    if [ "$bt_run" != "$em_run" ]; then
+        echo "[$file] FAIL: BT and emulation mismatch" >&2
+        echo "[BT result]"
+        echo "$bt_run"
+        echo "[Emulation result]"
+        echo "$em_run"
+        exit 1;
+    fi
+
+    echo "[$file] OK"
+}
+
+run_one sancheck "[00] 800003b8 [01] 00000718 [02] 00000718 [03] 00000718"
+run_one bench1   "[00] 00000001 [01] 00000367 [02] 00000001 [03] 00000367"
+run_one bench2   "[80000048] Done" "[00] 00000001 [01] 00000000 [02] 00000000 [03] 00000000"
+run_one bench3   "[80000038] Done" "[28] 80000c3c [29] 00000000 [30] 00000000 [31] 00000000"
+run_one bench4   "[00] 991727a0 [01] 5096a491 [02] 00000000 [03] 00000000"
